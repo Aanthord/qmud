dbb517daadb3bcc4412e07db290276d4f38d9184
Aanthord <michael.doran.808@gmail.com>
2025-08-19 16:26:01 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,5 +2,5 @@
 import { QuantumTruthMUD } from './game.js';
 
 const game = new QuantumTruthMUD();
-await game.init();           // top-level await is supported in ES modules on modern browsers
-window.game = game;          // expose for inline onclick attributes
+await game.init();
+window.game = game;
